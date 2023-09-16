4fa6631228930e007e1691498528501bc24093cc
crushdepth <simon@isengard.biz>
2023-09-16 09:32:36 +0700

    Content without expiry dates no longer affected.

--- trust_path/cron/expiresOn.php
+++ trust_path/cron/expiresOn.php
@@ -9,11 +9,12 @@ namespace Tfish;
  *
  * BONEHEAD WARNINGS:
  *
- * 1. Do not put this script in your web root, it should remain in an inaccessible location.
- * 2. If the system time is wrong, content with expiry dates could incorrectly be set offline.
+ * 1. DO NOT put this script in your web root, it should remain in a location inaccessible by browser.
+ * 2. Set the file ownership so that it can be run by the crontab but NOT by the webserver.
+ * 3. If the system time is wrong, content with expiry dates could incorrectly be set offline.
  *
- * In the interests of default security, the reference to mainfile.php is disabled by default to
- * prevent this script from running.
+ * In the interests of default security, the reference to mainfile.php is commented out by default
+ * to prevent this script from running.
  *
  * @copyright   Simon Wilkinson 2023+ (https://tuskfish.biz)
  * @license     https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html GNU General Public License (GPL) V2
@@ -34,7 +35,7 @@ $cache = $dice->create('\\Tfish\\Cache');
 $date = \date('Y-m-d', \time());
 
 // Count to determine if any rows are due for expiry.
-$sql = "SELECT COUNT(*) FROM `content` WHERE `expiresOn` > 0 AND `expiresOn` < :date AND `onlineStatus` = '1';";
+$sql = "SELECT COUNT(*) FROM `content` WHERE `expiresOn` != '' AND `expiresOn` < :date AND `onlineStatus` = '1';";
 $statement = $database->preparedStatement($sql);
 $statement->bindValue(':date', $date, \PDO::PARAM_STR);
 $statement->execute();
@@ -43,7 +44,7 @@ $count = (int) reset($count);
 
 // If any candidate rows were found, expire them and flush the cache.
 if ($count > 0) {
-    $sql = "UPDATE `content` SET `onlineStatus` = '0' WHERE `expiresOn` > '0' AND `expiresOn` < :date AND `onlineStatus` = '1';";
+    $sql = "UPDATE `content` SET `onlineStatus` = '0' WHERE `expiresOn` != '' AND `expiresOn` < :date AND `onlineStatus` = '1';";
     $statement = $database->preparedStatement($sql);
     $statement->bindValue(':date', $date, \PDO::PARAM_STR);
     $database->executeTransaction($statement);
