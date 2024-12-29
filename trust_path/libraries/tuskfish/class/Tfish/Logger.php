1c674fb8a4855166aef39c73e4f3108387518269
crushdepth <simon@isengard.biz>
2024-12-29 20:26:56 +0700

    Remove irrelevant docs.

--- trust_path/libraries/tuskfish/class/Tfish/Logger.php
+++ trust_path/libraries/tuskfish/class/Tfish/Logger.php
@@ -42,12 +42,7 @@ class Logger
     /**
      * Tuskfish custom error logger class.
      *
-     * Errors are logged to TFISH_ERROR_LOG_PATH (default is /trust_path/log/tuskfish_log.txt). For
-     * debugging purpose you can reverse the comment status of the last two lines to display errors
-     * on screen. Be aware, however, that this will prevent script execution from halting when an
-     * error is triggered, which has security implications. You must therefore CLOSE your site via
-     * the admin preferences before doing this. Comment the lines back out before re-opening your
-     * site.
+     * Errors are logged to TFISH_ERROR_LOG_PATH (default is /trust_path/log/tuskfish_log.txt).
      *
      * @param int|null $errno The level of the error raised.
      * @param string $error The error message.
@@ -71,10 +66,6 @@ class Logger
         $message .= "[$file:$line]\n";
         \error_log($message, 3, TFISH_ERROR_LOG_PATH);
 
-        // Debug only - comment OUT in production site to display errors on screen.
-        // echo '<p>' . print($message) . '</p>';
-
-        // Debug only - UNCOMMENT in production site.
         return false;
     }
 }
