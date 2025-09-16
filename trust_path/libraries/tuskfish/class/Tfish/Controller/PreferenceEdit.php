f56e41db546cc1fdca7795b67d525e26678afc26
crushdepth <simon@isengard.biz>
2025-09-16 14:59:11 +0700

    Detail PreferenceEdit controller.

--- trust_path/libraries/tuskfish/class/Tfish/Controller/PreferenceEdit.php
+++ trust_path/libraries/tuskfish/class/Tfish/Controller/PreferenceEdit.php
@@ -30,8 +30,8 @@ namespace Tfish\Controller;
 
 class PreferenceEdit
 {
-    private $model;
-    private $viewModel;
+    private object $model;
+    private object $viewModel;
 
     /**
      * Constructor
@@ -39,7 +39,7 @@ class PreferenceEdit
      * @param   object $model Instance of a model class.
      * @param   object $viewModel Instance of a viewModel class.
      */
-    public function __construct($model, $viewModel)
+    public function __construct(object $model, object $viewModel)
     {
         $this->model = $model;
         $this->viewModel = $viewModel;
@@ -78,6 +78,11 @@ class PreferenceEdit
      */
     public function update(): array
     {
+        if (($_SERVER['REQUEST_METHOD'] ?? '') !== 'POST') {
+            $this->viewModel->displayEdit();
+            return [];
+        }  
+
         $this->viewModel->displayUpdate();
 
         return [];
