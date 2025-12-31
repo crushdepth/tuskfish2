2e438c4311ea5bae74b596a970768fe0b1da0a6f
crushdepth <simon@isengard.biz>
2025-12-31 17:08:44 +1100

    Skeleton of FishStat module

---
+++ trust_path/libraries/tuskfish/class/Tfish/Fishstat/header.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Tuskfish header script for FishStat module.
+ *
+ * Sets additional routes and path constants.
+ *
+ * @copyright   Simon Wilkinson 2022+ (https://tuskfish.biz)
+ * @license     https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html GNU General Public License (GPL) V2
+ * @author      Simon Wilkinson <simon@isengard.biz>
+ * @version     Release: 2.0.4
+ * @since       2.0.4
+ * @package     User
+ */
+
+namespace Tfish\FishStat;
+
+// Make core language files available.
+include 'language/english.php';
+
+// Addtional routes for User module.
+$routingTable[
+    '/fishstat/'] = new \Tfish\Route(
+    '\\Tfish\\FishStat\\Model\\Listing',
+    '\\Tfish\\FishStat\\ViewModel\\Listing',
+    '\\Tfish\\View\\Listing',
+    '\\Tfish\\FishStat\\Controller\\Listing',
+    0);
+
+$routingTable[
+    '/admin/fishstat/'] = new \Tfish\Route(
+    '\\Tfish\\FishStat\\Model\\Admin',
+    '\\Tfish\\FishStat\\ViewModel\\Admin',
+    '\\Tfish\\FishStat\\Listing',
+    '\\Tfish\\FishStat\\Controller\\Admin',
+    1);
+
+// User file path constants.
+\define("TFISH_ADMIN_FISHSTAT_URL", TFISH_ADMIN_URL . 'fishstat/');
---
+++ trust_path/libraries/tuskfish/class/Tfish/Fishstat/language/english.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tfish\FishStat;
+
+/**
+ * Tuskfish User module language constants (English).
+ *
+ * Translate this file to convert Tuskfish User module to another language.
+ *
+ * @copyright   Simon Wilkinson 2022+ (https://tuskfish.biz)
+ * @license     https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html GNU General Public License (GPL) V2
+ * @author      Simon Wilkinson <simon@isengard.biz>
+ * @version     Release: 2.0
+ * @since       2.0
+ * @package     User
+ */
+// Language constants.
+\define("TFISH_FISHSTAT", "FishStat");
